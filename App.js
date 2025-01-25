ab97a6f13f1ba2129e6bdf46be4f5d91a546a8f2
roshan <roshan@neokred.tech>
2025-01-25 19:10:59 +0530

    feat: first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "Hello From h1 Tag"),
+        React.createElement("h2", {}, "Hello From h2 Tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "Hello From h1 Tag"),
+        React.createElement("h2", {}, "Hello From h2 Tag"),
+    ]),
+
+])
+
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
