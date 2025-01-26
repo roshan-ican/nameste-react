8606769959a27b307ea309acbc4da22066f992d3
roshan <roshan@neokred.tech>
2025-01-26 12:54:09 +0530

    fix: added react parcel and browserlist

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "Hello From h1 Tag"),
-        React.createElement("h2", {}, "Hello From h2 Tag"),
+        React.createElement("h5", {}, "Hello From h1 Tag"),
+        React.createElement("h2", {}, "This is nameste react "),
     ]),
     React.createElement("div", { id: "child2" }, [
         React.createElement("h1", {}, "Hello From h1 Tag"),
         React.createElement("h2", {}, "Hello From h2 Tag"),
-    ]),
 
+    ]),
 ])
 
 
--- Notes/Ep2.md
+++ Notes/Ep2.md
@@ -1,3 +1,17 @@
 # Package.json is a configuration for npm
 # dependencies and packages are same
 # react uses webpack (parcel, vite)
+- HMR = Hot Module Replacement
+- File Watching Algorithms - written in C++
+- Caching - Faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree shaking (removes unused code)
